fb76f1186f1c8d39a84360d6b1ffbc438d78fed2
Juarez at Home <juarez.asf@hotmail.com>
2013-12-01 19:31:38 -0200

    organizando

--- UnB2013/PVC/ProjetoFinal/build_all.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
- echo "Compilando  *.cpp"
-    g++ -ggdb ./src/*.cpp `pkg-config --cflags opencv` -o ./bin/ProjetoFinal $i `pkg-config --libs opencv` -I ./include/;
- echo "se tudo deu certo, o executável está em ./bin/"
