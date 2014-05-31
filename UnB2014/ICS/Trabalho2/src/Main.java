bb3cf499f35be50448ff12021872e2b76abb4812
Juarez@OperarionBase.ubunto.13-04 <juarez.asf@hotmail.com>
2014-05-31 14:01:21 -0300

    RAN implementado e testado

--- UnB2014/ICS/Trabalho2/src/Main.java
+++ UnB2014/ICS/Trabalho2/src/Main.java
@@ -1,12 +1,31 @@
 import sintese.*;
-import SomJASF.*;
-import SomJASF.Instrumentos.*;
+import SomJASF.RAN;
+import SomJASF.Instrumentos.Instrumento1;
+import SomJASF.Instrumentos.Instrumento2;
+import SomJASF.Instrumentos.Instrumento3;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Olá Mundo");
+		System.out.println("Teste RAN -- start");
+		
+		RAN rand = new RAN();
+		
+		rand.visualiza();
+		
+		try {
+		    Thread.sleep(3000);
+		} catch(InterruptedException ex) {
+		    Thread.currentThread().interrupt();
+		}
+		
+		rand.setA(10f);
+		rand.setF_ran(2);
+		rand.generateEnvoltoria();
+		rand.visualiza();
+		
+		System.out.println("Teste RAN -- fim");
 	}
 
 }
--- UnB2014/ICS/Trabalho2/src/SomJASF/RAN.java
+++ UnB2014/ICS/Trabalho2/src/SomJASF/RAN.java
@@ -1,11 +1,20 @@
 package SomJASF;
 
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
 import sintese.*;
+
+import org.math.plot.*;
+import org.math.plot.utils.Array;
 
 public class RAN extends Envoltoria{
 	
 	private float a, f_ran;
 	private int tamanho_curva;
+	private boolean set;
+	private int nPontos;
 	
 
 	//----------------------------------
@@ -19,8 +28,8 @@ public class RAN extends Envoltoria{
 	 * Seta novo tamanho da curva e gera nova envoltoria aleatória
 	 * @param tamanho_curva
 	 */
-	public void setTamanho_curva(int tamanho_curva) {
-		this.tamanho_curva = tamanho_curva;
+	public void setTamanho_curva(int new_tamanho_curva) {
+		this.tamanho_curva = new_tamanho_curva;
 		generateEnvoltoria();
 	}
 	
@@ -32,8 +41,8 @@ public class RAN extends Envoltoria{
 	 * seta o ganho para a
 	 * @param a
 	 */
-	public void setA(float a) {
-		this.a = a;
+	public void setA(float new_a) {
+		this.a = new_a;
 		setGanho(a);
 	}
 
@@ -45,8 +54,8 @@ public class RAN extends Envoltoria{
 	 * seta nova frequência de pontos de quebra e gera nova envoltória aleatória
 	 * @param f_ran
 	 */
-	public void setF_ran(float f_ran) {
-		this.f_ran = f_ran;
+	public void setF_ran(float new_f_ran) {
+		this.f_ran = new_f_ran;
 		generateEnvoltoria();
 	}
 	//----------------------------------
@@ -57,7 +66,7 @@ public class RAN extends Envoltoria{
 	 * de 60Hz. O tamanho da curva é setado para 720
 	 */
 	public RAN(){
-		setParameters(1f, 60, 720);
+		setParameters(1f, 60f, 720);
 	}
 	
 	public RAN(float new_a, float new_f_ran){
@@ -82,9 +91,11 @@ public class RAN extends Envoltoria{
 	 * @param tamanho_curva
 	 */
 	public void setParameters(float new_a, float new_f_ran, int tamanho_curva){
+		set = false;
 		setA(new_a);
 		setF_ran(new_f_ran);
 		setTamanho_curva(tamanho_curva);
+		set = true;
 		generateEnvoltoria();
 	}
 	
@@ -108,13 +119,55 @@ public class RAN extends Envoltoria{
 	 * o ganho é aplicado futuramente
 	 */
 	public void generateEnvoltoria(){
+		if (set == false) return;
 		Curva curv = new Curva(getTamanho_curva());
 		float amplitude = 1f;
 		for(float x = 0; x <= tamanho_curva; x+= tamanho_curva/f_ran){
 			curv.addPonto(x, getRandomNumber(amplitude));
 		}
 		
+		nPontos = (int)f_ran + 1;
+		
 		this.setCURVA(curv);
+	}
+	
+	
+	/**
+	 * Gera um gráfico da envoltória aleatória de -1 a +1 criada
+	 * considera que o tamanho máximo da tabela é 720
+	 * Se for usar mais do que isso irá dar pau! orriga se necessário!!
+	 */
+	public void visualiza(){
+		// create your PlotPanel (you can use it as a JPanel)
+		  Plot2DPanel plot = new Plot2DPanel();
+		 
+		  
+
+		  Tabela tab = this.getCURVA();
+		  
+		  ArrayList<Double> x = new ArrayList<Double>(); 
+		  ArrayList<Double> y = new ArrayList<Double>(); 
+		  for(int i = 0; i <= f_ran; i++){
+			  x.add((double )i*tamanho_curva/f_ran);
+			  y.add((double )tab.getValorNoIndice(i)*this.a) ;
+		  }
+		  
+		  // add a line plot to the PlotPanel
+		  double[] x_array =new double[x.size()];
+		  double[] y_array =new double[y.size()];
+		  for (int i = 0; i < x_array.length; i++) {
+			  x_array[i] = x.get(i).doubleValue();  // java 1.4 style
+			  y_array[i] = y.get(i).doubleValue();  // java 1.4 style
+		  }
+		  
+		  plot.addLinePlot("Rand Data", x_array, y_array);
+		 
+		  // put the PlotPanel in a JFrame, as a JPanel
+		  JFrame frame = new JFrame("a plot panel");
+		  frame.setContentPane(plot);
+		  frame.setVisible(true);
+		  frame.setSize(320, 240);
+		  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
 
